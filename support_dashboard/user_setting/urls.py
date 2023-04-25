37a6d01ce9047d885793baeef70e97822810e685
Kamlesh Chhipa <kamleshchhipa@kamleshchhipa.local>
2023-04-25 11:54:11 +0530

    added fcuntion to get api_key for an automation

--- support_dashboard/user_setting/urls.py
+++ support_dashboard/user_setting/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("settings", views.user_settings, name="user_settings"),
+    path("settings_fun_calls", views.settings_fun_calls, name="settings_fun_calls"),
 ]
--- support_dashboard/user_setting/views.py
+++ support_dashboard/user_setting/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from .models import AutomationCredentials
@@ -32,3 +33,35 @@ def user_settings(request):
         return render(
             request, "user_settings.html", {"credentials_list": credentials_list}
         )
+
+
+def get_api_key_for_user_and_name(user, name):
+    try:
+        credentials = AutomationCredentials.objects.get(user=user, name=name)
+        return credentials.api_key
+    except AutomationCredentials.DoesNotExist:
+        return None
+
+
+@login_required
+def settings_fun_calls(request):
+    if request.method == "POST":
+        action = request.POST.get("update_zendesk")
+        param1 = request.POST.get("param1")
+        # call the appropriate function based on the value of `action`
+        if action == "update_zendesk":
+            user = request.user
+            api_key_name = action
+            api_key_value = get_api_key_for_user_and_name(user, api_key_name)
+            result = api_key_value if api_key_value is not None else "API key not found"
+            return HttpResponse(result)
+        elif action == "function2":
+            # result = function2()
+            pass
+        else:
+            result = "Unknown action"
+        return HttpResponse(result)
+    else:
+        # handle GET requests
+        # render the form
+        return render(request, "ui.html")
