c639e8ed26daada4084b6057931d953f828d3396
Kamlesh Chhipa <kamleshchhipa@kamleshchhipa.local>
2023-05-03 07:05:45 +0530

    added vercel config file to deploy

--- support_dashboard/support_dashboard/settings.py
+++ support_dashboard/support_dashboard/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-2=&9ip4eihmx!(bccjfo#-9#yzd8qi9#+@3of(mifwa)@*5-y4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
