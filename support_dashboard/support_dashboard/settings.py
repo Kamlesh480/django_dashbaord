286bb1c1b51362289eb09ab877c33ff9897506fc
Kamlesh Chhipa <kamleshchhipa@kamleshchhipa.local>
2023-05-03 10:35:24 +0530

    update setting

--- support_dashboard/support_dashboard/settings.py
+++ support_dashboard/support_dashboard/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-2=&9ip4eihmx!(bccjfo#-9#yzd8qi9#+@3of(mifwa)@*5-y4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
