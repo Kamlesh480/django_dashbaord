30d7d3bb398a0d328b94df2d3172b292f38cf6b9
Kamlesh Chhipa <kamleshchhipa@kamleshchhipa.local>
2023-05-03 10:16:07 +0530

    deply on render cloud

--- support_dashboard/support_dashboard/urls.py
+++ support_dashboard/support_dashboard/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("zendesk_ticket_update.urls")),
     path("", include(("user_setting.urls", "user_setting"), namespace="settings")),
 ]
+
+
+urlpatterns += staticfiles_urlpatterns()
