9cc15abaab57a968cf5c35b1b9fcd603d0e2f7d5
Kamlesh Chhipa <kamleshchhipa@kamleshchhipa.local>
2023-05-05 09:59:05 +0530

    added error massage http response in case of zendesk api failture

--- test.py
+++ test.py
@@ -1,45 +1,68 @@
-all_custom_fields = [
-    "new_issue",
-    "follow_up_issue",
-    "not_an_issue",
-    "reopen_after_autoclose",
-    "not_a_reopen",
-    "reopen_issue_resolved_additonal_info",
-]
+import requests
+import json
+from credentials import (
+    headers,
+    all_custom_fields,
+    load,
+    base_url,
+    additional_tag,
+    custom_fields,
+    payload,
+)
 
 
-while True:
-    print(
-        "Enter a number between 1 and {} to select a custom field:".format(
-            len(all_custom_fields) + 1
-        )
-    )
-    for index, field in enumerate(all_custom_fields):
-        print("{}. {}".format(index + 1, field))
-    print("{}. Other".format(len(all_custom_fields) + 1))
+def making_zendes_api_calls(ticket_id, custom_field, api_key_value):
+    url = base_url.format(ticket_id)
 
-    selection = input()
+    # update headers for api_key_value
+    headers["Authorization"] = "Basic " + api_key_value
+    tags = ""
+    # load = load
 
-    if not selection.isdigit():
-        print("Oops! That was not a valid number. Please try again...")
-    elif int(selection) < 1 or int(selection) > len(all_custom_fields) + 1:
-        print("Oops! Number not in range. Please try again...")
-    elif selection == "" or selection == str(len(all_custom_fields) + 1):
-        custom_field = None
-        print("You have selected: Other")
-        break
-    else:
-        custom_field = all_custom_fields[int(selection) - 1]
-        print("You have selected: {}".format(custom_field))
-        break
+    print("My header: {}".format(headers))
 
+    try:
+        print("Fetching ticket {}: Start".format(ticket_id))
+        response = requests.request("GET", url, headers=headers)
+        print(url)
+        print(headers)
 
-all_custom_fields = [
-    "Other",
-    "new_issue",
-    "follow_up_issue",
-    "not_an_issue",
-    "reopen_after_autoclose",
-    "not_a_reopen",
-    "reopen_issue_resolved_additonal_info",
-]
+        if response.status_code == 200:
+            print("Fetching ticket {}: Done".format(ticket_id))
+            data = json.loads(response.text)
+            tags = data["ticket"]["tags"]
+            tags.append(additional_tag)
+
+        else:
+            print(f"Failed to fetch ticket Error code: {response.status_code}")
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
+
+    load["ticket"]["tags"] = tags
+    # optimze the call
+    if custom_field in all_custom_fields and custom_field != "no update needed":
+        load["ticket"]["custom_fields"] = custom_fields
+        load["ticket"]["custom_fields"][0]["value"] = custom_field
+        # print("called if block")
+
+    payload = json.dumps(load)
+    print("Update ticket {}: Start".format(ticket_id))
+    print("pyload----------------------")
+    print(payload)
+    print("----------------------pyload")
+
+    # # API PUT CALL
+    try:
+        print("Update ticket {}: Start".format(ticket_id))
+        response = requests.request("PUT", url, headers=headers, data=payload)
+        if response.status_code == 200:
+            print("Updating ticket {}: Done".format(ticket_id))
+
+        else:
+            print(
+                "Updating Ticket: Failed \n Error code: {}".format(response.status_code)
+            )
+
+    except requests.exceptions.RequestException as e:
+        print(f"An error occurred: {e}")
