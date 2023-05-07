4887a26b8753a741583bb8bc9e2e297253ff51ff
Kamlesh Chhipa <kamleshchhipa@kamleshchhipa.local>
2023-05-07 06:26:51 +0530

    removed registration page

--- support_dashboard/zendesk_ticket_update/urls.py
+++ support_dashboard/zendesk_ticket_update/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from . import views
+from django.views.generic import TemplateView, RedirectView
 
 urlpatterns = [
-    path("login", views.login, name="login"),
     path("", views.login, name="login"),
-    path("register", views.register, name="register"),
+    path("login", RedirectView.as_view(permanent=True, url="/")),
+    path("register", RedirectView.as_view(permanent=True, url="/")),
+    # path("register", views.register, name="register"),
     path("home", views.home, name="home"),
     path("update_zendesk", views.update_zendesk, name="update_zendesk"),
     path("call_zendesk_api", views.call_zendesk_api, name="call_zendesk_api"),
